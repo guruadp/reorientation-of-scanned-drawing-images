bbf231b082c1d399aff47f855840a852559668ee
guruadp <gurunandhanadp@gmail.com>
2022-06-29 16:40:24 +0530

    Added OCR angle detection

--- main_program.py
+++ main_program.py
@@ -6,6 +6,9 @@ import math
 import pytesseract
 import re
 import glob
+import imutils
+
+pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
 
 from pdf2image.exceptions import (
         PDFInfoNotInstalledError,
@@ -57,18 +60,37 @@ def angle_detection_hough_line(image):
         cv2.line(image,(x1,y1),(x2,y2),(0,255,0),2)
         angle = math.atan2(y2 - y1, x2 - x1) 
         angle = round(math.degrees(angle), 1)
-        angle = angle if (angle>0) else angle + 90
+        # angle = angle if (angle>0) else angle + 90
         angles.append(angle)
     
     #cv2.imshow('Lines', image)
-    print(angles)
     count = {i: angles.count(i) for i in angles}
-    print(count)
     hough_angle = list(count.keys())[list(count.values()).index(max(count.values()))]
     return hough_angle
+
+def save_file(filename, img_rotated):
+    filename = str(filename.split(".")[0]) + "_rotated.png"
+    cv2.imwrite(filename, img_rotated)
 
 file = "sample.pdf"
 image = pdf_to_image(file)
 hough_angle = angle_detection_hough_line(image)
-print(f'Angle = {hough_angle}')
+print(f'Hough angle = {hough_angle}')
+
+image = cv2.imread(image)
+gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+results = pytesseract.image_to_osd(file.split('.')[0]+'.jpg')
+# display the orientation information
+ocr_angle = re.search('(?<=Orientation in degrees: )\d+', results).group(0)
+
+ocr_angle = float(ocr_angle)
+print(f'OCR angle = {ocr_angle}')
+rotated = imutils.rotate_bound(image, angle=ocr_angle)
+cv2.imshow('OCR rotated', rotated)
+rotated = imutils.rotate_bound(image, angle=hough_angle*(-1))
+cv2.imshow('Hough rotated', rotated)
+# rotated_image = cv2.rotate(image, rotateCode=) #image.rotate(ocr_angle, fillcolor="white", expand=True)
+rotated_image = np.array(rotated)
+save_file(file, rotated_image)
 cv2.waitKey(0)
